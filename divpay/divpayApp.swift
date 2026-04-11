3c18f96fc0371acf15a617900b1a186a97fd0ef2
yabao <154517591+vngbh@users.noreply.github.com>
2026-04-11 17:07:24 +0900

    Initial Commit

---
+++ divpay/divpayApp.swift
@@ -0,0 +1,17 @@
+//
+//  divpayApp.swift
+//  divpay
+//
+//  Created by GIA BAO HOANG on 2026/04/11.
+//
+
+import SwiftUI
+
+@main
+struct divpayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
